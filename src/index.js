e9b0ee434ae3f53344c8887efb8284ff7169c1c5
Zebedee Lewis <zebedeeslewis@gmail.com>
2020-09-28 10:11:42 -0600

    Setup Main Slider Pagination

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import Splide from '@splidejs/splide';
 import './scss/index.scss'
 
 
-const MAIN_HOME_SLIDER_CLASSNAME = 'home-slider--main'
+const MAIN_HOME_SLIDER_CLASSNAME = 'main-slider'
+const NAV_SLIDER_CLASSNAME = 'main-slider__pagination'
 
-const mainHomeSlider =
+const mainSlider =
   new Splide
     ( `.${MAIN_HOME_SLIDER_CLASSNAME}`
-    , { autoplay : true
-      , type     : 'loop'
+    , { autoplay   : true
+      , cover      : true
+      , autoWidth  : true
+      , autoHeight : true
+      , type       : 'loop'
+      , arrows     : false
+      , pagination : false
+      }
+    )
+
+const mainSliderPagination =
+  new Splide
+    ( `.${NAV_SLIDER_CLASSNAME}`
+    , { autoWidth    : true
+      , autoHeight   : true
+      , isNavigation : true
+      , arrows       : false
+      , pagination   : false
       }
     ).mount()
+
+mainSlider.sync(mainSliderPagination).mount()
