1427e56e910170c36fb3b2e3d96642586c9e3f57
Zebedee Lewis <zebedeeslewis@gmail.com>
2020-10-25 15:45:59 -0600

    Setup Controller Model

--- src/modules/Controller/Controller.ts
+++ src/modules/Controller/Controller.ts
@@ -1,5 +1,133 @@
 
+import * as I from "immutable"
+
+import type {  RecordOf, Record } from 'immutable'
+
+
+interface FailureInterface<Model, Error>
+  { error  : Error
+  , model  : Model
+  }
+
+
+
+/** Provides information about a failed model update operation. */
+type Failure<Model, Error> = RecordOf< FailureInterface<Model, Error> >
+
+
+
+type FailureFactory =
+  ( data : Partial< FailureInterface<any, any> > ) => Failure<any, any>
+
+
+
+export const Failure : FailureFactory =
+  I.Record
+    ( { error : 'initial'
+      , model : 'tempModel'
+      }
+    , 'Failure'
+    )
+
+
 
 /**
- * The Controller is 
+ * Interface for a function that knows how to update a model
+ * of a given type
  */
+export type Updater<Model, Message> =
+  ( message : Message, model : Model ) => Model
+
+
+
+const dummyUpdater : Updater<any, any> =
+  ( message
+  , model
+  ) => model
+
+
+
+interface ControllerInterface<Model, Message>
+  { model   : Model
+  , updater : Updater<Model, Message>
+  }
+
+
+
+/**
+ * The Controller is a command (i.e. Message) reciever and dispatcher
+ */
+export type Controller<Model, Message> =
+  RecordOf< ControllerInterface<Model, Message> >
+
+
+
+type ControllerFactory =
+  ( data : Partial< ControllerInterface<any, any> >
+  ) => Controller<any, any>
+
+
+
+export const Controller : ControllerFactory =
+  I.Record
+    ( { model   : 'tempModel'
+      , updater  : dummyUpdater
+      }
+    , 'Controller'
+    )
+
+
+
+export function get_model<Model, Message>
+  ( controller : Controller<Model, Message>
+  ) : Model {
+    return controller.get('model', undefined)
+  }
+
+
+
+export function set_model<Model, Message>
+  ( newModel   : Model
+  , controller : Controller<Model, Message>
+  ) : Controller<Model, Message> {
+    return controller.set('model', newModel)
+  }
+
+
+
+export function get_updater<Model, Message>
+  ( controller : Controller<Model, Message>
+  ) : Updater<Model, Message> {
+    return controller.get('updater', undefined)
+  }
+
+
+
+export function set_updater<Model, Message>
+  ( newUpdater : Updater<Model, Message>
+  , controller : Controller<Model, Message>
+  ) : Controller<Model, Message> {
+    return controller.set('updater', newUpdater)
+  }
+
+
+
+export function dispatch_message<Model, Message>
+  ( message    : Message
+  , controller : Controller<Model, Message>
+  ) : Controller<Model, Message> {
+    const update_model = get_updater(controller)
+
+    return (
+      set_model
+        ( update_model
+            ( message
+            , get_model(controller)
+            )
+        , controller
+        )
+    )
+  }
+
+
+
