0280b45aba1b40abb5d5dab391b56023ddd93d92
Zebedee Lewis <zebedeeslewis@gmail.com>
2025-11-04 10:57:17 -0600

    docs(build): Document Webpack production configuration components
    Added extensive comments to `build/config/webpack.prod.js`.

--- build/config/webpack.prod.js
+++ build/config/webpack.prod.js
@@ -6,36 +6,38 @@ const webpackCore = require('./webpack.core.js')
 const ProjectDesc = require('../lib/ProjectDesc')
 const buildConfig = require('./build.config.js')
 
-
-
 const projectDesc = ProjectDesc.create(buildConfig)
 
-const webpackModule =
-  { rules:
-      [ { test: /\.s?css$/i
-        , use:
-            [ MiniCssExtractPlugin.loader
-            , 'css-loader'
-            , { loader  : 'postcss-loader'
-              , options : 
-                  { postcssOptions :
-                    { config: ProjectDesc.get_postCssConfig(projectDesc)
-                    }
-                  }
-              }
-            , 'resolve-url-loader'
-            , { loader  : 'sass-loader'
-              , options :
-                  { sourceMap : true
-                  }
-              }
-            ]
-        }
-      ]
-  }
+const webpackModule = {
+  rules: [
+    {
+      // Matches all CSS/SCSS files
+      test: /\.s?css$/i,
+      use: [
+        // Use MiniCssExtractPlugin loader instead of 'style-loader'
+        // This extracts CSS from the JS bundle into a separate physical CSS file.
+        MiniCssExtractPlugin.loader,
 
+        // The rest of the loaders (css-loader, postcss-loader,
+        // resolve-url-loader, sass-loader) remain the same as dev configuration
+        // but ensure they run before extraction.
+        { loader: 'css-loader', options: { sourceMap: false } },
+        { loader: 'postcss-loader', options: {
+            sourceMap: false,
+            postcssOptions: {
+              // Assumes ProjectDesc provides the postcss config path
+              config: ProjectDesc.get_postCssConfig(projectDesc),
+            },
+          },
+        },
+        { loader: 'resolve-url-loader', options: { sourceMap: false } },
+        { loader: 'sass-loader', options: { sourceMap: true } },
+      ],
+    },
+  ],
+}
 
-
+// Extract CSS into separate files defined by the output path.
 const plugins =
   [ new MiniCssExtractPlugin
       ( { filename : path.join('css', '[name].css')
@@ -43,13 +45,9 @@ const plugins =
       )
   ]
 
-
-
 const prodConfig =
   { module: webpackModule
   , plugins
   }
-
-
 
 module.exports = merge(webpackCore, prodConfig)
